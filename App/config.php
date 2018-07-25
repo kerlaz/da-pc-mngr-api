f4a7d9466c22bdce136a797176754c9d308b392c
inkoda <inkodaa@gmail.com>
2018-07-25 10:43:19 +0500

    beta build 0.1.1

--- App/config.php
+++ App/config.php
@@ -4,22 +4,34 @@ session_start();
 
 require __DIR__ . "/../vendor/autoload.php";
 
+$db['dev'] = [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'da_db',
+    'username' => 'root',
+    'password' => 'n0smi1ez',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
+$db['prod'] = [
+    'driver' => 'mysql',
+    'host' => 'srv-pleskdb17.ps.kz:3306',
+    'database' => 'dobrayaa_slim',
+    'username' => 'dobra_user',
+    'password' => 'Geed43*8',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
+
 $app = new Slim\App([
     'settings' => [
         'determineRouteBeforeAppMiddleware' => true,
         'displayErrorDetails' => true,
         'addContentLengthHeader' => false,
-        'db' => [
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'database' => 'da_db',
-            'username' => 'root',
-            'password' => 'n0smi1ez',
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-        ],
-        'jwt_secret'=>'528C17F906A642F0EB1FB26A7ABBCB60D4B3319979C95F9046D38D2A0FC2FA33'
+        'db' => $db['prod'],
+        'jwt_secret' => '528C17F906A642F0EB1FB26A7ABBCB60D4B3319979C95F9046D38D2A0FC2FA33'
     ],
 ]);
 
